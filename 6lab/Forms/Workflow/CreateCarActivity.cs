fbc8f751d7cc3ad109679c9aa98c5385e9447189
replicaCortex <replicaCortex@gmail.com>
2025-02-25 08:34:10 +0300

    6lab beta

---
+++ 6lab/Forms/Workflow/CreateCarActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Activities;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using dll;
+
+namespace Forms
+{
+    public sealed class CreateCarActivity : CodeActivity
+    {
+        public InArgument<int> id { get; set; } // Или long, если id большой
+        public InArgument<string> Model { get; set; }
+        public InArgument<string> Metal { get; set; }
+        public InArgument<int> Age { get; set; }
+        public InArgument<bool> Break { get; set; }
+        public OutArgument<Car> CustomCar { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            int ageValue = context.GetValue(Age);
+
+            Car car = new Car
+            {
+                Age = ageValue
+            };
+
+            CustomCar.Set(context, car);
+        }
+    }
+}
